348b8b77cde3d3bdee50e48f51a36d07cd9f1419
TestBird_Mac <405260188@qq.com>
2024-05-11 18:39:33 +0800

    新东方部分

---
+++ 新东方/readme/readme.md
@@ -0,0 +1,5 @@
+目标：
+1. 地址：https://souke.xdf.cn/search?cityCode=510100&kw=%E8%8B%B1%E8%AF%AD
+
+2. 任务：获取Sign值构建
+
---
+++ 新东方/xdf.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Author : John
+# @Time : 2024/05/11
+# @File : xdf.py
+
+import requests
+
+
+headers = {
+    "authority": "dsapi.xdf.cn",
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "application/json",
+    "origin": "https://souke.xdf.cn",
+    "referer": "https://souke.xdf.cn/search?cityCode=510100&kw=%E8%8B%B1%E8%AF%AD",
+    "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sign": "65f06073d4ad01169cd4eee87edcd48d",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+url = "https://dsapi.xdf.cn/product/v2/class/search"
+params = {
+    "appId": "5053",
+    "cityCode": "510100",
+    "t": "1715417538014",
+    "pageIndex": "2",
+    "pageSize": "12",
+    "keyword": "英语",
+    "order": "0"
+}
+response = requests.get(url, headers=headers, params=params)
+
+print(response.text)
+# print(response)
