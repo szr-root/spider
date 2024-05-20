d79714dd5ed767567b69d6ff6055296bf1b6adfd
TestBird_Mac <405260188@qq.com>
2024-05-20 15:01:37 +0800

    增加常用代理配置

---
+++ 常用工具/补环境/代理配置.js
@@ -0,0 +1,28 @@
+
+// ============================ 代理 ============================
+function SetProxy(proxyObjs) {
+    for (let i = 0; i < proxyObjs.length; i++) {
+        const handler = `{
+          get: function(target, property, receiver) {
+          if (property!="Math" && property!="isNaN"){
+             if (target[property] && typeof target[property] !="string" &&  Object.keys(target[property]).length>3){
+              }else{
+            console.log("方法:", "get  ", "对象:", "${proxyObjs[i]}", "  属性:", property, "  属性类型：", typeof property, ", 属性值：", target[property]);}}
+            return target[property];
+          },
+          set: function(target, property, value, receiver) {
+            console.log("方法:", "set  ", "对象:", "${proxyObjs[i]}", "  属性:", property, "  属性类型：", typeof property, ", 属性值：", value, ", 属性值类型：", typeof target[property]);
+            return Reflect.set(...arguments);
+          }
+        }`;
+        eval(`try {
+            ${proxyObjs[i]};
+            ${proxyObjs[i]} = new Proxy(${proxyObjs[i]}, ${handler});
+        } catch (e) {
+            ${proxyObjs[i]} = {};
+            ${proxyObjs[i]} = new Proxy(${proxyObjs[i]}, ${handler});
+        }`);
+    }
+}
+
+SetProxy(["window","document","navigator","location","localStorage"])
